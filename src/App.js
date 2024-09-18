0999abab8b8e6a9628b67db7847d2dbddf84e7ea
Rey Echeverria <rey.echeverria7@gmail.com>
2024-09-18 00:47:10 -0700

    Front end app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import Nav from "./components/nav";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route
+            path="/listaRefacciones"
+            element={<ListaRefacciones />}
+          ></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
+}
+
+export function ListaRefacciones() {
+  const [refacciones, setRefacciones] = useState([]);
+
+  const getRefacciones = async () => {
+    await axios
+      .get("http://localhost:8000/refacciones/api/v1/refacciones")
+      .then((res) => {
+        setRefacciones(res.data);
+      });
+  };
+
+  useEffect(() => {
+    getRefacciones();
+  }, []);
+
+  console.log(refacciones);
+  return (
+    <>
+      <h2>Lista de Refacciones</h2>
+      <div className="listaRefacciones">
+        {refacciones.map((refaccion) => (
+          <Refaccion refaccion={refaccion} />
+        ))}
+      </div>
+    </>
+  );
+}
+
+export function Refaccion({ refaccion }) {
+  return (
+    <div className="refaccionCard">
+      <img src={refaccion.imagePath} alt="" />
+      <CardDescription refaccion={refaccion} />
+    </div>
+  );
+}
+
+export function CardDescription({ refaccion }) {
+  return (
+    <div className="cardDescription">
+      <p>{refaccion.nombre}</p>
+      <p className="priceTag">${refaccion.precio}</p>
+      <p>{refaccion.codigo}</p>
+    </div>
+  );
+}
+
+export function Home() {
+  return <h1>Home</h1>;
 }
 
 export default App;
---
+++ src/components/nav.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "../App.css";
+import { Link } from "react-router-dom";
+
+function Nav() {
+  return (
+    <div>
+      <header className="header">
+        <div className="logo">
+          <Link to="/">
+            <img src="msr_logo.PNG" alt="MSR Logo" />
+          </Link>
+        </div>
+        <nav>
+          <ul className="nav-links">
+            <li>
+              <Link to="/listaRefacciones">Refacciones</Link>
+            </li>
+            <li>
+              <Link to="/">Nosotros</Link>
+            </li>
+          </ul>
+        </nav>
+        <a href="" className="btn">
+          <button>Iniciar Sesion</button>
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Nav;
