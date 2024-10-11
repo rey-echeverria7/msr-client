630f207c2738e472d2a699af3ad0557d4db2cd0d
Rey Echeverria <rey.echeverria7@gmail.com>
2024-10-10 17:32:38 -0700

    Added hero in home component

--- src/App.js
+++ src/App.js
@@ -169,7 +169,34 @@ export function CardDescription({ refaccion }) {
   */
 
 export function Home() {
-  return <h1>Home</h1>;
+  return (
+    <section class="section-hero">
+      <div className="hero">
+        <div className="hero-text-box">
+          <h1 className="heading-primary">
+            Mecanicos hay muchos. <br></br>Con experiencia pocos. <br></br>Como
+            nosotros ninguno.
+          </h1>
+
+          <p className="hero-description">
+            Taller mecánico y venta de refacciones dedicado al mantenimiento y
+            reparación de motocicletas de trabajo. <br></br>
+            <br></br> Tabasco 1246 entre Aureliano Anaya y Flavio Borquez,
+            Colonia Campestre, Ciudad Obregón, Mexico
+          </p>
+          <a href="#" className="btn btn--full margin-right-sm">
+            Contacto
+          </a>
+          <a href="#" className="btn btn--outline">
+            motoservicioruelas@gmail.com &darr;
+          </a>
+        </div>
+        <div className="hero-img-box">
+          <img src="static/hero2.jpg" className="hero-img" alt="Taller" />
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export function AgregarRefaccion() {
